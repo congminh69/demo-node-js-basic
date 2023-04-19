8d61b5cfcc5b169457de5b375f85fe01123f8198
congminh <congminh699669@gmail.com>
2023-04-19 23:54:12 +0700

    init api

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send("Hello world !");
-})
+const port = 3000;
 
-app.listen(3000,()=>{
-    console.log("Start with port 3000");
-})
+// Route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/api/calculate', (req, res) => {
+  const { num1, num2, operator } = req.body;
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      result = 'Invalid operator';
+  }
+
+  res.send({ result });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
